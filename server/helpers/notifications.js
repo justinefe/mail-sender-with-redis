b989bdedb83f9255dddcfa33ed3fee7163c680b7
Justin <efejustin3@gmail.com>
2021-01-30 10:04:33 +0100

    second the committ

--- server/helpers/notifications.js
+++ server/helpers/notifications.js
@@ -7,7 +7,7 @@ import template from './template';
  * @param {*} emailTo
  * @param {*} link
  * @param {*} name
- * @returns {*} sends an email to a new user
+ * @returns {*} sends an email to a new users
  */
 const registrationEmail = (emailTo, link, name) => {
   const subject = 'Welcome to Bull';
